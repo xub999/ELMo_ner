bd851b687ab7c14c4bdfcf2ed1dfe66bd133c091
Barton <barton_xu@trendmicro.com>
2019-03-26 09:53:41 +0800

    [mod]train model complete

--- ner.py
+++ ner.py
@@ -21,6 +21,7 @@ elmo['have_trained_nb_epoch_file'] = dir + "record/have_trained_nb_epoch.dat.npy
 elmo['tensorboard_dir'] = dir + "record/tensorboard"
 elmo['hub_model_file'] = dir + "record/hub_elmo_module"
 elmo['model_h5'] = dir + "record/keras_model/"
+elmo['val_metrics_file'] = dir + "record/val_metrics.dat"
 
 elmo['batch_size'] = 96
 elmo['maxlen'] = 50
@@ -29,7 +30,8 @@ elmo['test_rate'] = 0.1
 elmo['val_rate'] = 0.1
 elmo["n_epochs"] = 20
 
-elmo['n_tags'] = 0
+elmo['tags'] = ['O', 'I-eve', 'I-tim', 'I-org', 'I-gpe', 'B-tim', 'B-eve', 'I-per', 'B-gpe', 'B-per', 'B-geo', 'I-geo', 'I-nat', 'B-org', 'I-art', 'B-nat', 'B-art']
+elmo['n_tags'] = len(elmo['tags'])
 
 # for google drive
 
@@ -50,8 +52,14 @@ from keras.models import Model, Input, load_model
 from keras.layers.merge import add
 from keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional, Lambda
 from keras.preprocessing.sequence import pad_sequences
+from keras import metrics
 import random
 
+
+def my_train_test_split(X, y, split_rate):
+    split_index = int(X.shape[0] * (1.0-split_rate))
+    print("split_index = " + str(split_index))
+    return X[:split_index], X[split_index:], y[:split_index], y[split_index:]
 
 class SentenceGetter(object):
     def __init__(self, data):
@@ -85,8 +93,7 @@ class Data(object):
 
         words = list(set(data["Word"].values))
         words.append("ENDPAD")
-        tags = list(set(data["Tag"].values))
-        elmo['n_tags'] = len(tags)
+        tags = elmo['tags']
         getter = SentenceGetter(data)
 
         sentences = getter.sentences
@@ -106,17 +113,17 @@ class Data(object):
         y = [[tag2idx[w[2]] for w in s] for s in sentences]
         y = pad_sequences(maxlen=max_len, sequences=y, padding="post", value=tag2idx["O"])
 
-        X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=test_split_rate, random_state=2018)
+        # X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=test_split_rate, random_state=2018)
+        X_tr, X_te, y_tr, y_te = my_train_test_split(X, y, test_split_rate)
         X_tr = np.array(X_tr)
         self.X_te = np.array(X_te)
 
-        self.X_tr, self.X_val, y_tr, y_val = train_test_split(X_tr, y_tr, test_size=val_split_rate, random_state=2018)
-        # self.X_tr, self.X_val = X_tr[:1213 * self.batch_size], X_tr[-135 * self.batch_size:]
-        # y_tr, y_val = y_tr[:1213 * self.batch_size], y_tr[-135 * self.batch_size:]
-        self.X_tr = self.X_tr[:4 * self.batch_size]
-        self.X_val = self.X_val[-4 * self.batch_size:]
-        y_tr = y_tr[:4 * self.batch_size]
-        y_val = y_val[-4 * self.batch_size:]
+        # self.X_tr, self.X_val, y_tr, y_val = train_test_split(X_tr, y_tr, test_size=val_split_rate, random_state=2018)
+        self.X_tr, self.X_val, y_tr, y_val = my_train_test_split(X_tr, y_tr, val_split_rate)
+        # self.X_tr = self.X_tr[:4 * self.batch_size]
+        self.X_val = self.X_val[:self.X_val.shape[0]//self.batch_size * self.batch_size]
+        # y_tr = y_tr[:4 * self.batch_size]
+        y_val = y_val[:y_val.shape[0]//self.batch_size * self.batch_size]
 
         self.y_tr = y_tr.reshape(y_tr.shape[0], y_tr.shape[1], 1)
         self.y_val = y_val.reshape(y_val.shape[0], y_val.shape[1], 1)
@@ -167,6 +174,79 @@ from keras.callbacks import Callback, ModelCheckpoint, TensorBoard
 from keras.utils import plot_model
 
 
+def my_sparse_categorical_accuracy(y_true, y_pred):
+    return K.mean(
+        K.cast(K.equal(K.flatten(y_true), K.flatten(K.cast(K.argmax(y_pred, axis=-1), K.floatx()))), K.floatx()))
+
+
+from sklearn.metrics import f1_score, precision_score, recall_score
+import pickle
+
+
+val_metrics = dict()
+
+
+class My_Metrics(Callback):
+    def __init__(self, val_data):
+        super(My_Metrics, self).__init__()
+
+        self.val_data = val_data
+        self.val_metrics = dict()
+        if os.path.exists(elmo['val_metrics_file']):
+            with open(elmo['val_metrics_file'], 'rb') as f:
+                self.val_metrics = pickle.load(f)
+        else:
+            self.val_metrics = {"val_f1s": [], "val_recalls": [], "val_precisions": [],
+                                "micro_val_f1s": [], "micro_val_recalls": [], "micro_val_precisions": []}
+
+        self.val_f1s = self.val_metrics["val_f1s"]
+        self.val_recalls = self.val_metrics["val_recalls"]
+        self.val_precisions = self.val_metrics["val_precisions"]
+        self.micro_val_f1s = self.val_metrics["micro_val_f1s"]
+        self.micro_val_recalls = self.val_metrics["micro_val_recalls"]
+        self.micro_val_precisions = self.val_metrics["micro_val_precisions"]
+
+    def on_epoch_end(self, epoch, logs=None):
+        val_predict = np.expand_dims(np.argmax(np.asarray(self.model.predict(self.val_data[0], batch_size=elmo['batch_size'], verbose=1)), -1).flatten(), -1)
+        val_targ = np.expand_dims(self.val_data[1].flatten(), -1)
+
+        _val_f1 = f1_score(val_targ, val_predict, average=None)
+        _val_recall = recall_score(val_targ, val_predict, average=None)
+        _val_precision = precision_score(val_targ, val_predict, average=None)
+        self.val_f1s.append(_val_f1)
+        self.val_recalls.append(_val_recall)
+        self.val_precisions.append(_val_precision)
+        print("val_f1s =")
+        for fls in self.val_f1s:
+            print("\t".join(["%.4f" % i for i in fls]))
+        print("val_recalls =")
+        for recalls in self.val_recalls:
+            print("\t".join(["%.4f" % i for i in recalls]))
+        print("val_precisions =")
+        for precisions in self.val_precisions:
+            print("\t".join(["%.4f" % i for i in precisions]))
+
+        _val_f1 = f1_score(val_targ, val_predict, average='micro')
+        _val_recall = recall_score(val_targ, val_predict, average='micro')
+        _val_precision = precision_score(val_targ, val_predict, average='micro')
+        self.micro_val_f1s.append(_val_f1)
+        self.micro_val_recalls.append(_val_recall)
+        self.micro_val_precisions.append(_val_precision)
+        print("\nmicro_val_f1s = \t" + "\t".join(["%.4f" % i for i in self.micro_val_f1s]))
+        print("micro_val_recalls = \t" + "\t".join(["%.4f" % i for i in self.micro_val_recalls]))
+        print("micro_val_precisions = \t" + "\t".join(["%.4f" % i for i in self.micro_val_precisions]))
+
+        self.val_metrics["val_f1s"] = self.val_f1s
+        self.val_metrics["val_recalls"] = self.val_recalls
+        self.val_metrics["val_precisions"] = self.val_precisions
+        self.val_metrics["micro_val_f1s"] = self.micro_val_f1s
+        self.val_metrics["micro_val_recalls"] = self.micro_val_recalls
+        self.val_metrics["micro_val_precisions"] = self.micro_val_precisions
+
+        with open(elmo['val_metrics_file'], 'wb') as f:
+            pickle.dump(self.val_metrics, f)
+
+
 class Save_crt_epoch_nb(Callback):
     def __init__(self, filepath):
         super(Save_crt_epoch_nb, self).__init__()
@@ -185,10 +265,31 @@ class Save_keras_model(Callback):
         tf.contrib.saved_model.save_keras_model(self.model, self.filepath)
 
 
+class Save_records(Callback):
+    def __init__(self):
+        super(Save_records, self).__init__()
+        # self.t_logs = t_logs
+        # self.v_logs = v_logs
+
+    def on_epoch_end(self, epoch, logs=None):
+        print('logs = ' + str(logs))
+        '''
+        print('epoch %d:\tloss: %.5f\t6_dice: %.4f' %
+              (epoch, logs['loss'],
+               logs['dice_coef_except_background']))
+        print('\t\t\tval_loss: %.5f\tval_6_dice: %.4f' %
+              (logs['val_loss'],
+               logs['val_dice_coef_except_background']))
+        print()
+        '''
+
+
 elmo_model = None
 if os.path.exists(elmo['hub_model_file']):
+    print("load hub model from file")
     elmo_model = hub.Module(elmo['hub_model_file'])
 else:
+    print("load hub model from URL")
     elmo_model = hub.Module("https://tfhub.dev/google/elmo/2", trainable=True)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
@@ -213,12 +314,16 @@ class ELMo(object):
 
         # load elmo model
         self.elmo_net = None
-        self.elmo_net = self.get_elmo()
+        # self.elmo_net = self.get_elmo()
         model_path = config['elmo']['modelCheckpoint_file'] if USE_checkpoint_model else config['elmo']['model_h5']
         if os.path.exists(model_path):
-            print(model_path + "exists")
+            print(model_path + " exists")
             if USE_checkpoint_model:
-                self.elmo_net = load_model(model_path, custom_objects={'elmo_mode': elmo_model})
+                global elmo_model
+                self.elmo_net = load_model(model_path, custom_objects={'elmo_model': elmo_model, 'tf': tf, 'elmo': elmo,
+                                                                       'my_sparse_categorical_accuracy': my_sparse_categorical_accuracy})
+                # self.elmo_net = load_model(model_path)
+                self.elmo_net.summary()
                 # self.elmo_net.load_weights(model_path)
             else:
                 self.elmo_net = tf.contrib.saved_model.load_keras_model(elmo['model_h5'])
@@ -227,7 +332,7 @@ class ELMo(object):
             print('loading elmo model and weights from file')
             print("got elmo")
         else:
-            # self.elmo_net = self.get_elmo()
+            self.elmo_net = self.get_elmo()
             self.elmo_net.summary()
             print('no elmo model file exists, creating model')
 
@@ -273,7 +378,7 @@ class ELMo(object):
 
         model = Model(input_text, out)
         # model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-        model.compile(optimizer="adam", loss="sparse_categorical_crossentropy")
+        model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=[my_sparse_categorical_accuracy])
 
         return model
 
@@ -286,6 +391,11 @@ class ELMo(object):
                                        verbose=1, save_best_only=False, save_weights_only=False)
         tensorboard = TensorBoard(log_dir=config['elmo']['tensorboard_dir'])
 
+        val_data = [np.array(self.myData.X_val), np.array(self.myData.y_val)]
+        my_metrics = My_Metrics(val_data)
+
+        saveRecords = Save_records()
+
         print('Fitting model...')
 
         elmo_net.fit_generator(
@@ -294,9 +404,9 @@ class ELMo(object):
             epochs=self.epoches,
             verbose=1,
             # callbacks=[save_crt_epoch_nb, checkpointer, tensorboard],
-            callbacks=[save_crt_epoch_nb, checkpointer, tensorboard],
-            validation_data=self.generator_data_validate_fine(),
-            validation_steps=self.myData.total_nb_batch_validate,
+            callbacks=[my_metrics, save_crt_epoch_nb, checkpointer, tensorboard],
+            # validation_data=self.generator_data_validate_fine(),
+            # validation_steps=self.myData.total_nb_batch_validate,
             class_weight=None,
             max_queue_size=10,
             workers=1,
